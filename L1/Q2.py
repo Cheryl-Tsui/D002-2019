c86a165fc1ac671b46b80b9ed552c0e9828076b1
Cheryl-Tsui <53845623+Cheryl-Tsui@users.noreply.github.com>
2019-08-08 22:19:49 +0800

    Add files via upload

---
+++ L1/Q2.py
@@ -0,0 +1,12 @@
+from math import*
+print(8*3.57)
+print(5+30*20)
+print((5+30)*20)
+print(1+(2+20*3)/(4*2))
+print(1+2**10)
+print(ceil(29/4))
+
+radius=input("What is the radius?")
+int(radius)
+radius=sqrt(100/pi)
+print((2*radius+1)**2)
