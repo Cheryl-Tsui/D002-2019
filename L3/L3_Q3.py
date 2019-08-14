e5d38bd66508fd96d108663205a63179912d2ae6
Cheryl-Tsui <53845623+Cheryl-Tsui@users.noreply.github.com>
2019-08-14 10:25:31 +0800

    Add files via upload

---
+++ L3/L3_Q3.py
@@ -0,0 +1,10 @@
+word=[]
+for i in range (0,10):
+    x = input("Please enter a word")
+    n = x[0]
+    if n== "a" or n== "e" or n== "i" or n== "o" or n== "u":
+        word.append(n)
+print(word)
+
+
+    
